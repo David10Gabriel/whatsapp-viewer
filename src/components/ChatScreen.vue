52fd5e2b302224f38a40016f8cc75d984d8cef96
flushin <lcsbetel@hotmail.com>
2019-08-21 20:55:27 -0300

    Feature - Separação das mensagens por esquerda e direita

--- src/components/ChatScreen.vue
+++ src/components/ChatScreen.vue
@@ -4,14 +4,14 @@
       <v-flex xs12>
         <v-card>
           <v-card-text>
-          <v-flex v-for="mensagem in arrayConversa" :key="mensagem.texto" :text-right="mensagem.texto.includes('David')">
+          <v-flex v-for="mensagem in arrayConversa" :key="mensagem.texto" :text-right="mensagem.posicao">
             <v-tooltip top>
               <template v-slot:activator="{ on }">
-                <v-chip v-if="mensagem.texto.includes('David')" color="darkgrey" pill v-on="on"> 
-                  {{ mensagem.texto }}<v-avatar right color="blue">P</v-avatar>
+                <v-chip v-if="mensagem.posicao" color="darkgrey" pill v-on="on"> 
+                  {{ mensagem.texto }}<v-avatar right color="blue">{{mensagem.remetente[0]}}</v-avatar>
                 </v-chip>
                 <v-chip v-else color="darkgrey" pill v-on="on"> 
-                  <v-avatar left color="blue">P</v-avatar>{{ mensagem.texto }}
+                  <v-avatar left color="red">{{mensagem.remetente[0]}}</v-avatar>{{ mensagem.texto }}
                 </v-chip>
               </template>
               <span>{{ mensagem.remetente }}</span>
@@ -52,12 +52,19 @@ export default {
       this.arrayConversa = this.arrayConversa.split("\n")
       let arrayAuxiliar = []
 
+      let ultimoRemetente = { nome: null, posicao: true}
+
       this.arrayConversa.forEach(mensagem => {
+        let remetenteAtual = mensagem.includes(":") && mensagem.includes("]") ? mensagem.split(":")[2].split("]")[1].trimLeft() : "Whatsapp"
         arrayAuxiliar.push({
+          original: mensagem, 
           texto: mensagem.split(":").slice(3, 10000).join(),
-          data: mensagem.split("]")[0].replace("[", "")
-          // remetente: mensagem.split(":")[2].split("]")[1],
+          data: mensagem.split("]")[0].replace("[", "").trimLeft(),
+          remetente: remetenteAtual,
+          posicao: ultimoRemetente.nome === remetenteAtual ? ultimoRemetente.posicao : !ultimoRemetente.posicao
         })
+
+        ultimoRemetente = { nome: remetenteAtual, posicao: ultimoRemetente.nome === remetenteAtual ? ultimoRemetente.posicao : !ultimoRemetente.posicao}
       });
 
       this.arrayConversa = arrayAuxiliar
